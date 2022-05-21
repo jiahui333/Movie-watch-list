681a1d9ed6c831ae4904ddc973bc46cf42df76ae
jiahui <forestlin333@gmail.com>
2022-05-22 00:25:27 +0200

    added list style

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,19 +24,16 @@ class ListsController < ApplicationController
   def destroy
     @list = List.find(params[:id])
     @list.destroy
-    redirect_to lists_path
+    redirect_to root_path
   end
 
   private
 
-  # # def set_task
-  # #   @task = Task.find(params[:id])
-  # # end
-
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
   def bookmark_params
     params.require(:bookmark).permit(:comment)
   end
+
 end
